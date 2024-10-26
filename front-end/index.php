50fdec25f6b888a105a9a6842ee4a91f485b6d59
Lucas Pedrozo <161781256+lucas-pedrozo@users.noreply.github.com>
2024-10-25 23:18:33 -0300

    criacao do index

---
+++ front-end/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TECNOLO</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+    <!-- aos master -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+            <script> AOS.init(); </script>
+
+</head>
+
+<header>
+
+<div class="nav_mobile">
+
+</div>
+
+<div class="nav_desk">
+    
+</div>
+
+</header>
+
+<body>
+
+    <main>
+        <?php
+        if (isset($_GET["param"])) {
+            $param = $_GET["param"];
+            $p = explode("/", $param);
+        }
+
+        $page = $p[0] ?? "home";
+
+        if ($page == "home") {
+            $pagina = "paginas/{$page}.php";
+        }
+
+        if (file_exists($pagina)) {
+            include $pagina;
+        } else {
+            include "paginas/erro.php";
+        }
+        ?>
+    </main>
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("meu nome é lucas pedroizo");
