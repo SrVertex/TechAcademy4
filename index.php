d37dc7c3c4dd7b6bf5194db5e14ef3c4802d53c0
Lucas Pedrozo <161781256+lucas-pedrozo@users.noreply.github.com>
2024-10-30 19:42:34 -0300

    tela de cadastro

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
         <title>TECNOLO</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+            rel="stylesheet">
 
         <link rel="stylesheet" href="css/style.css">
         <base href="http://localhost/techAcademy4/index.php">
@@ -43,7 +44,9 @@
                         <a href=""><i class="bi bi-universal-access-circle"></i></a>
                         <a href="carrinho.php"><i class="bi bi-cart"></i></a>
 
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                     </div>
@@ -52,8 +55,10 @@
 
                 <form class="d-flex" role="search">
                     <input class="2" type="search" placeholder="Digite o que você procura..." aria-label="Search">
-                    <a href="" type="submit"> <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
-                            <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1" />
+                    <a href="" type="submit"> <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                            class="bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1" />
                         </svg></a>
                 </form>
 
@@ -62,20 +67,24 @@
                     <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
 
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
                                 Menu
                             </a>
 
                             <ul class="dropdown-menu dropdown-menu-1 ">
                                 <li><a class="dropdown-item" href="#"><i class="bi bi-cart"></i>Carrinho</a></li>
-                                <li><a class="dropdown-item" href="#"><i class="bi bi-bag-fill"></i>Meus Pedidos</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="bi bi-bag-fill"></i>Meus Pedidos</a>
+                                </li>
                                 <li><a class="dropdown-item" href="#"><i class="bi bi-whatsapp"></i>WhatzApp</a></li>
-                                <li><a class="dropdown-item" href="#"><i class="bi bi-house-door-fill"></i>Meu Endereço</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="bi bi-house-door-fill"></i>Meu
+                                        Endereço</a></li>
                             </ul>
                         </li>
 
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
                                 Categorias
                             </a>
 
@@ -120,7 +129,7 @@
     <body>
 
         <main>
-        <?php
+            <?php
 
 if (isset($_GET["param"])) {
   $param = $_GET["param"];
--- paginas/cadastro.php
+++ paginas/cadastro.php
@@ -1,3 +1,22 @@
 <div class="conteiner_cadastro">
-    
+
+    <h2 class="text-center">
+        CRIE SUA CONTA TECNOLO
+    </h2>
+
+    <form action="">
+
+        <div class="conteiner_input">
+            <input type="text" placeholder="E-mail">
+            <input type="text" placeholder="Senha">
+            <input type="text" placeholder="Confimar Senha">
+            <input type="text" placeholder="Nome Completo">
+        </div>
+
+        <div class="btn_login">
+            <button>Voltar</button>
+            <button>Confirmar</button>
+        </div>
+    </form>
+
 </div>
