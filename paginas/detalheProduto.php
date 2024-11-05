4c3c502b47988a558f011d7e7b04cb8c46adf259
Lucas Pedrozo <161781256+lucas-pedrozo@users.noreply.github.com>
2024-11-04 22:19:09 -0300

    tela de detalhe a produto 80Por

--- paginas/detalheProduto.php
+++ paginas/detalheProduto.php
@@ -22,20 +22,77 @@
                 <hr>
             </div>
 
-                <div class="compra">
-                    <div class="conteiner_preco">
-                        <p class="preco">R$ 78.000,00</p>
-                        <p class="subPreco">Aviste no Pix ou até <span>10x de R$7.800,00</span> sem juros</p>
-                    </div>
+            <div class="compra">
+                <div class="conteiner_preco">
+                    <p class="preco">R$ 78.000,00</p>
+                    <p class="subPreco">Aviste no Pix ou até <span>10x de R$7.800,00</span> sem juros</p>
+                </div>
 
-                    <div class="compra_botao">
-                        <button class="comprar_cart"><i class="bi bi-cart-plus"></i> COMPRAR</button>
-                        <button class="carrinho"><i class="bi bi-cart-plus"></i></button>
+                <div class="compra_botao">
+                    <button class="comprar_cart"><i class="bi bi-cart-plus"></i> COMPRAR</button>
+                    <button class="carrinho"><i class="bi bi-cart-plus"></i></button>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="cep">
+                <div class="busca">
+                    <form action="">
+                        <input type="text" placeholder="Insira o CEP">
+                        <button>OK</button>
+                    </form>
+                </div>
+
+                <a type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Não lembro meu CEP
+                </a>
+
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h2 class="modal-title" id="exampleModalLabel">Busque seu CEP</h2>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <hr>
+
+            </div>
+
+            <div class="descricao_completa">
+                <p>iPhone 16 Pro Max. Com estrutura em titânio, Controle da Câmera, 4K Dolby Vision a 120 qps e o chip A18 Pro. Avisos legais *As telas têm bordas arredondadas. Quando medida como um retângulo, a tela tem 6,12 polegadas (iPhone 16), 6,69 polegadas (iPhone 16 Plus), 6,27 polegadas (iPhone 16 Pro) ou 6,86 polegadas (iPhone 16 Pro Max) na diagonal. A área real de visualização é menor.</p>
+            </div>
         </div>
 
+    </div>
+
+    <div class="conteiner_ficha">
+
+        <h2>Ficha Técnica</h2>
+        
+        <hr>
+
+        <div class="tributo">
+            <p>Marca: Apple</p>
+        </div>
+        <div class="tributo">
+            <p>Modelo: iPhone 16 pro max</p>
+        </div>
+        <div class="tributo">
+            <p>Armazenamento: 512 Gb</p>
+        </div>
+        <div class="tributo">
+            <p>Ram: 8 Gb</p>
+        </div>
+        <div class="tributo">
+            <p>Tamanho:  9 polegadas</p>
+        </div>
     </div>
 
 
