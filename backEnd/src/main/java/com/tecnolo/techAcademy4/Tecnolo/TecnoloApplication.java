c75d874adbd57315a0e208d318b7319a98beba87
SrVertex_Notebook <srvertex@outlook.com>
2024-11-06 19:52:14 -0300

    Criacao Spring

---
+++ backEnd/src/main/java/com/tecnolo/techAcademy4/Tecnolo/TecnoloApplication.java
@@ -0,0 +1,13 @@
+package com.tecnolo.techAcademy4.Tecnolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnoloApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/com/tecnolo/techAcademy4/Tecnolo/TecnoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnolo.techAcademy4.Tecnolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
