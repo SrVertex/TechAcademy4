ff9a1ab8b5e46cbdebd531c65dfce452e889777e
SrVertex_Notebook <srvertex@outlook.com>
2024-11-11 19:53:38 -0300

    f

---
+++ Tecnolo/src/main/java/com/backEnd/Tecnolo/TecnoloApplication.java
@@ -0,0 +1,13 @@
+package com.backEnd.Tecnolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnoloApplication.class, args);
+	}
+
+}
