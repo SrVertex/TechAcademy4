d364dddc5d5e7f76fc46cddfe531a5c85ded5296
Lucas Pedrozo <161781256+lucas-pedrozo@users.noreply.github.com>
2024-12-02 11:31:33 -0300

    ajuste

---
+++ backEnd/Tecnolo/src/main/java/com/backEnd/Tecnolo/dto/UsuarioJuri_RequestDTO.java
@@ -0,0 +1,14 @@
+package com.backEnd.Tecnolo.dto;
+
+public class UsuarioJuri_RequestDTO extends Usuario_RequestDTO {
+
+    private String juritico;
+
+    public String getJuritico() {
+        return juritico;
+    }
+
+    public void setJuritico(String juritico) {
+        this.juritico = juritico;
+    }
+}
